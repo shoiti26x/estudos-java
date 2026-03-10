ccb9a9200fe37a1eb2b5870678a805de81bb9708
Matheus Shoiti Tokunaga <tokusho76@gmail.com>
2026-03-09 21:56:30 -0300

    initial commit

---
+++ src/br/com/alura/screenmatch/Principal/PrincipalComListas.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch.Principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        meuFilme.avalia(6);
+        Filme filmeDoPaulo = new Filme("dogville", 2003);
+        meuFilme.avalia(7);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item : lista) {
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item;
+            System.out.println("Classificação " + filme.getClassificacao());
+
+        }
+    }
+}
